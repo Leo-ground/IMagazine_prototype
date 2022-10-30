9090b6dc6717ba9203cd4804ed08dde96bd315ff
Leo-ground <piscesljy@gmail.com>
2022-10-30 11:20:03 +0900

    JS: navbar click re-fec

--- main.js
+++ main.js
@@ -19,7 +19,11 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null ){
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(link);
+    
 })
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
