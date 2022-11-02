1a1b90c219d9961c285f360b591371766955e467
Leo-ground <piscesljy@gmail.com>
2022-11-02 19:49:09 +0900

    contents slide set

--- main.js
+++ main.js
@@ -34,3 +34,33 @@ const aboutHeight = about.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     about.style.opacity = 1-scrollY/aboutHeight;
 })
+
+
+//slide format
+const slides = document.querySelector('.contents__slide');
+const slideImg = document.querySelectorAll('.contents__slide div');
+let currentIdx = 0;
+const slideCount = slideImg.length;
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slideWidth = 300;
+const slideMargin = 100;
+
+slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+
+function moveSlide(num) {
+    slides.style.left = -num * 400 + 'px';
+    currentIdx = num;
+}
+
+prev.addEventListener('click', () => {
+    if(currentIdx !==0) {
+        moveSlide(currentIdx-1);
+    }
+});
+
+next.addEventListener('click', () => {
+    if(currentIdx !== slideCount -1) {
+        moveSlide(currentIdx +1);
+    }
+})
